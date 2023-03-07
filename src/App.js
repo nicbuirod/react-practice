d905149f5adb52aa0d3eb9f97d2af9643e076742
Nicolas <nicbuirod@hotmail.com>
2023-03-06 19:45:57 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from "react-redux";
+import { setMessage, clearMessage } from "./redux/actions/messageAction";
 
-function App() {
+function App(props) {
+  console.log(props);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{props.message}</h1>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setMessage: (payload) => dispatch(setMessage(payload)),
+  clearMessasge: () => dispatch(clearMessage()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
-import messageReducer from "reducers/messageReducer";
-function configureStore(state = { messsage: "Default message" }) {
+
+import messageReducer from "./reducers/messageReducer";
+function configureStore(state = { message: "Default message" }) {
   return createStore(messageReducer, state);
 }
 export default configureStore();
